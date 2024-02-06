9852a64c2e0e2c082c1933d96356b4f8cd3ddacd
sanket rahangdale <sanketrahangdale1118@gmail.com>
2024-02-06 18:21:48 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 // src/main.jsx or src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css'; // Import your styles
+// import './styles.css'; // Import your styles
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
